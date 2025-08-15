6fa7f1325c12dec977c073417860c966fbbe29dd
keyiflerolsun <keyiflerolsun@gmail.com>
2025-08-15 17:08:54 +0300

    refactor: remove echo_string and print_env

--- minishell/builtins/echo.c
+++ minishell/builtins/echo.c
@@ -6,58 +6,11 @@
 /*   By: osancak <osancak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 14:59:18 by osancak           #+#    #+#             */
-/*   Updated: 2025/08/14 21:43:34 by osancak          ###   ########.fr       */
+/*   Updated: 2025/08/15 17:04:29 by osancak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
-
-static void	print_env(char *token, char **ep)
-{
-	char	*tmp;
-	int		i;
-
-	i = 0;
-	while (token[i] && token[i] != '$')
-		i++;
-	token[i] = '\0';
-	tmp = ft_strjoin(token, "=");
-	while (*ep)
-	{
-		if (!ft_strncmp(*ep, tmp, ft_strlen(tmp)))
-		{
-			ft_printf("%s", *ep + ft_strlen(tmp));
-			break ;
-		}
-		ep++;
-	}
-	free(tmp);
-	token[i] = '$';
-}
-
-static void	echo_string(char *token, t_vars vars)
-{
-	while (*token)
-	{
-		if (*token == '$' && token++)
-		{
-			if (*token == '?' && token++)
-				ft_printf("%d", vars.last_exit_code);
-			else if (*token == '$' && token++)
-				ft_printf("%d", getpid());
-			else if (*token == '0' && token++)
-				ft_printf("minismet");
-			else
-			{
-				print_env(token, vars.ep);
-				while (*token && *token != '$')
-					token++;
-			}
-		}
-		else
-			ft_printf("%c", *token++);
-	}
-}
 
 static int	n_is_valid(char *arg)
 {
@@ -78,11 +31,12 @@ void	ft_echo(char **tokens, t_vars vars)
 
 	args = tokens + 1;
 	print_newline = 1;
+	(void)vars;
 	while (*args && n_is_valid(*args) && args++)
 		print_newline = 0;
 	while (*args)
 	{
-		echo_string(*args, vars);
+		ft_printf("%s", *args);
 		if (*(args + 1))
 			ft_printf(" ");
 		args++;
