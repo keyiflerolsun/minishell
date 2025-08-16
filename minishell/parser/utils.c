692d8324b9901be45644ea023bf000d79b23d01a
osancak <osancak@student.42istanbul.com.tr>
2025-08-16 20:56:31 +0300

    expander norm

--- minishell/parser/utils.c
+++ minishell/parser/utils.c
@@ -6,7 +6,7 @@
 /*   By: osancak <osancak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 18:42:41 by osancak           #+#    #+#             */
-/*   Updated: 2025/08/16 20:49:06 by osancak          ###   ########.fr       */
+/*   Updated: 2025/08/16 20:55:38 by osancak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,24 @@ char	*ft_strjointwo(char *s1, char *s2)
 	str[total_len] = '\0';
 	free(s1);
 	return (str);
+}
+
+char	*ft_strjoin_three(char first, char *middle, char last)
+{
+	size_t	len;
+	char	*res;
+
+	if (!middle)
+		return (NULL);
+	len = ft_strlen(middle) + 2;
+	res = malloc(len + 1);
+	if (!res)
+		return (NULL);
+	res[0] = first;
+	ft_memcpy(res + 1, middle, ft_strlen(middle));
+	res[len - 1] = last;
+	res[len] = '\0';
+	return (res);
 }
 
 char	*get_env_value(char **envp, char *key)
