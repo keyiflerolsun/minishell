756c0928c715495fac3d9af52ae03e52eecd3393
osancak <osancak@student.42istanbul.com.tr>
2025-08-28 18:18:04 +0300

    errno-base

--- minishell/executor/err_exit.c
+++ minishell/executor/err_exit.c
@@ -6,19 +6,31 @@
 /*   By: osancak <osancak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 14:41:18 by osancak           #+#    #+#             */
-/*   Updated: 2025/08/28 17:50:30 by osancak          ###   ########.fr       */
+/*   Updated: 2025/08/28 18:17:11 by osancak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "executor.h"
 
-static void	ft_errno_manipulate(t_vars *vars, int ex)
+static void	ft_errno_manipulate(t_vars *vars, char *left)
 {
+	int ex;
+
+	ex = vars->last_exit_code;
+	if (ex == 127)
+	{
+		write_err(left, "Command not found\n");
+		exit(ex);
+	}
+	if (ex == ENOENT)
+	{
+		vars->last_exit_code = 127;
+		write_err(left, "No such file or directory\n");
+		exit(vars->last_exit_code);
+	}
 	if (ex == ENOTDIR || ex == EACCES)
 		vars->last_exit_code = 126;
-	if (ex == ENOENT)
-		vars->last_exit_code = 127;
 }
 
 void	error_exit(char *left, char *right)
@@ -27,10 +39,8 @@ void	error_exit(char *left, char *right)
 
 	vars = static_vars(NULL);
 	vars->last_exit_code = errno;
-	ft_errno_manipulate(vars, vars->last_exit_code);
-	if (vars->last_exit_code == 127)
-		write_err(left, "Command not found\n");
-	else if (!right)
+	ft_errno_manipulate(vars, left);
+	if (!right)
 		write_perr(left);
 	else
 		write_err(left, right);
