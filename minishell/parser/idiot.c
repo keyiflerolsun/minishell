c5e3368d7caae63e149eabb09586aad36b7ac5b2
Halit Yakici <hyakici@k1m14s02.42istanbul.com.tr>
2025-08-31 17:26:12 +0300

    idiot.c

---
+++ minishell/parser/idiot.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   idiot.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyakici <hyakici@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/31 17:17:56 by hyakici           #+#    #+#             */
+/*   Updated: 2025/08/31 17:19:30 by hyakici          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+int	cont_meta(t_vars *vars, char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		if (ft_is_op(args[i][0]) && ft_is_op(args[i + 1][0]))
+		{
+			vars->last_exit_code = 333;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
