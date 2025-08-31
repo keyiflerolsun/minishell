d841eb1c6fe721b106d0e3e40dd47029106cbde1
Halit Yakici <hyakici@k1m14s02.42istanbul.com.tr>
2025-08-31 18:30:55 +0300

    selam sana imparator

--- minishell/parser/idiot.c
+++ minishell/parser/idiot.c
@@ -6,7 +6,7 @@
 /*   By: hyakici <hyakici@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 17:17:56 by hyakici           #+#    #+#             */
-/*   Updated: 2025/08/31 17:19:30 by hyakici          ###   ########.fr       */
+/*   Updated: 2025/08/31 18:29:17 by hyakici          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	cont_meta(t_vars *vars, char **args)
 	i = 0;
 	while (args[i])
 	{
-		if (ft_is_op(args[i][0]) && ft_is_op(args[i + 1][0]))
+		if (ft_is_op(args[i][0]) && (!args[i + 1] || ft_is_op(args[i + 1][0])))
 		{
 			vars->last_exit_code = 333;
 			return (1);
