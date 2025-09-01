8b16ad0b66b83b36a04f34f0a76b079e0571be06
osancak <osancak@student.42istanbul.com.tr>
2025-09-01 16:03:45 +0300

    nesrim usta

--- minishell/builtins/cd.c
+++ minishell/builtins/cd.c
@@ -6,13 +6,13 @@
 /*   By: osancak <osancak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 16:44:32 by osancak           #+#    #+#             */
-/*   Updated: 2025/08/27 10:39:34 by osancak          ###   ########.fr       */
+/*   Updated: 2025/09/01 16:03:27 by osancak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-static int	ft_cd_check(t_vars *vars, char **tokens, char *buff)
+static int	ft_cd_check(t_vars *vars, char **tokens)
 {
 	if (!tokens[1])
 	{
@@ -20,21 +20,16 @@ static int	ft_cd_check(t_vars *vars, char **tokens, char *buff)
 		vars->last_exit_code = 42;
 		return (0);
 	}
-	if (tokens[2])
+	else if (tokens[2])
 	{
 		write_err("cd", "too many arguments\n");
 		vars->last_exit_code = 2;
 		return (0);
 	}
-	if (chdir(tokens[1]) != 0)
+	else if (chdir(tokens[1]) != 0)
 	{
 		write_perr(tokens[1]);
 		vars->last_exit_code = errno;
-		return (0);
-	}
-	if (!getcwd(buff, sizeof(buff)))
-	{
-		vars->last_exit_code = EXIT_FAILURE;
 		return (0);
 	}
 	return (1);
@@ -45,8 +40,13 @@ void	ft_cd(char **tokens, t_vars *vars)
 	char	*old;
 	char	buff[1024];
 
-	if (!ft_cd_check(vars, tokens, buff))
+	if (!ft_cd_check(vars, tokens))
 		return ;
+	if (!getcwd(buff, sizeof(buff)))
+	{
+		vars->last_exit_code = EXIT_FAILURE;
+		return ;
+	}
 	old = get_env(vars->env, "PWD");
 	update_env(&vars->env, "OLDPWD", old);
 	free(old);
